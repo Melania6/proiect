e3551480fa6b1e6124707be1e1433dcdcc999cd5
Melania6 <97430919+Melania6@users.noreply.github.com>
2022-01-21 11:17:05 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("ana")
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("mere")
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("are")
+    basic.pause(200)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
